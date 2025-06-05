38c151ea94a49c465a0ec2711dfc154f51a82773
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-06-06 11:45:58 +1200

    array of array's

--- ML/web/js/draw.js
+++ ML/web/js/draw.js
@@ -1,7 +1,7 @@
 const draw={};
 
 draw.path=(ctx,path,color="black")=>{
-    ctx.strokestyle=color;
+    ctx.strokeStyle = color;
     ctx.lineWidth=3;
     ctx.beginPath();
     ctx.moveTo(...path[0]);
@@ -11,4 +11,10 @@ draw.path=(ctx,path,color="black")=>{
     ctx.lineCap="round";
     ctx.lineJoin="round";
     ctx.stroke();
+}
+
+draw.paths=(ctx,paths,color="black")=>{
+    for(const path of paths){
+        draw.path(ctx,path,color);
+    }
 }
