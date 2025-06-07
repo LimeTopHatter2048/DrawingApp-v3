3926718496cf5339d975b561bb9eab054004ac7c
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-06-07 12:39:34 +1200

    EXTRACTING FEATURES

--- ML/common/constants.js
+++ ML/common/constants.js
@@ -7,6 +7,7 @@ constants.JSON_DIR=constants.DATASET_DIR+"/json";
 constants.IMG_DIR=constants.DATASET_DIR+"/img";
 
 constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
+constants.FEATURES=constants.DATASET_DIR+"/features.json";
 constants.JS_OBJECTS="../common/js_objects";
 constants.SAMPLES_JS=constants.JS_OBJECTS+"/samples.js";
 
