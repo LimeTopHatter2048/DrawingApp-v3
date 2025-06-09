4011c231284a59153cb54bdb78badf34d429fa6d
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-06-09 16:00:11 +1200

    graphics.js -Draws a filled circle on the canvas at loc
    this.pixelBounds
    -Margins around the canvas to avoid edge-clipping.
    
    this.dataBounds
    -Determines min/max X and Y of the data.
    
    #drawSamples()
    -Transforms data points into canvas coordinates and plots them.
    
    math.remap(...) (inside #drawSamples)	Converts data coordinates to pixel space for drawing.
    
    graphics.drawPoint(...)	Plots a circle for each sample on the chart.

--- CUSTOM CHART/chart.js
+++ CUSTOM CHART/chart.js
@@ -15,5 +15,55 @@ class Chart{
 
         this.margin=options.size*0.1;
         this.transparency=0.5;
+
+        this.pixelBounds=this.#getPixelBounds();
+        this.dataBounds=this.#getDataBounds();
+
+        this.#draw();
+    }
+    #getPixelBounds(){
+        const {canvas,margin}=this;
+        const bounds={
+            left:margin,
+            right:canvas.width-margin,
+            top:margin,
+            bottom:canvas.height-margin
+        };
+        return bounds;
+    }
+    #getDataBounds(){
+        const {samples}=this;
+        const x=samples.map(s=>s.point[0]);
+        const y=samples.map(s=>s.point[1]);
+        const minX=Math.min(...x);
+        const maxX=Math.max(...x);
+        const minY=Math.min(...y);
+        const maxY=Math.max(...y);
+        const bounds={
+            left:minX,
+            right:maxX,
+            top:maxY,
+            bottom:minY
+        };
+        return bounds;
+    }
+    #draw(){
+        const {ctx,canvas}=this;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+        ctx.globalAlpha=this.transparency;
+        this.#drawSamples();
+        ctx.globalAlpha=1;
+    }
+    #drawSamples(){
+        const {ctx,samples,dataBounds,pixelBounds}=this;
+        for(const sample of samples){
+            const {point}=sample;
+            const pixelLoc=[
+                math.remap(dataBounds.left,dataBounds.right,pixelBounds.left,pixelBounds.right,point[0]),
+                math.remap(dataBounds.top,dataBounds.bottom,pixelBounds.top,pixelBounds.bottom,point[1])
+            ];
+            graphics.drawPoint(ctx,pixelLoc);
+        }
     }
 }
