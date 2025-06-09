8f6bce7e2ade627e132e5fce06a518d0106c7044
Leonaitasi Mafileo <leonaitasim@gmail.com>
2025-06-09 16:11:45 +1200

    fix: math.PI to Math.PI

--- CUSTOM CHART/graphics.js
+++ CUSTOM CHART/graphics.js
@@ -3,6 +3,6 @@ const graphics={};
 graphics.drawPoint=(ctx,loc,color="black",size=8)=>{
     ctx.beginPath();
     ctx.fillStyle=color;
-    ctx.arc(...loc,size/2,0,math.PI*2);
+    ctx.arc(...loc,size/2,0,Math.PI*2);
     ctx.fill();
 }
